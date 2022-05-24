30eb066630737eeefef0b5e4e0731d36985ef797
Yurii <yuramasha13@gmail.com>
2022-05-24 21:43:05 +0300

    Adds SimpleLightbox & smooth scroll

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,26 +1,25 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const KEY = '27495256-af825ffcf26d4c7b8b5ea146c';
+const URL = 'https://pixabay.com/api/';
 
+const customAxios = axios.create({
+  baseURL: URL,
+  params: {
+    key: KEY,
+  },
+});
 
-
-export const getData = async (query, page=1) => {
+export const getData = async params => {
   try {
-    
-    const { data } = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: KEY,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        per_page: 40,
-        page: page,
-        safesearch: 'true',
-      },
+    const { data } = await customAxios.get('', {
+      params,
     });
 
     return data;
   } catch (error) {
+    Notiflix.Notify.failure(`Smth went wrong ${error}`);
     throw new Error(`Smth went wrong ${error}`);
   }
 };
--- src/js/components/pixaby/index.js
+++ src/js/components/pixaby/index.js
@@ -1,8 +1,19 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { getData } from '../../api';
-import cardsTemplate from '../../../js/templates/photo-card.hbs';
+import cardsTemplate from '../../templates/photo-card.hbs';
 
 const PER_PAGE = 40;
+const fetchParams = {
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: PER_PAGE,
+  page: 1,
+  safesearch: 'true',
+};
 
 let lastQuery;
 let page = 1;
@@ -11,6 +22,12 @@ let refs = {
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 function refreshTemplate() {
   refs.gallery.innerHTML = '';
@@ -44,28 +61,48 @@ let searchHandler = evt => {
     refs.loadMoreBtn.classList.toggle('load-more--hidden');
   }
 
-  getData(refs.form.elements.searchQuery.value).then(cards => {
+  fetchParams.q = refs.form.elements.searchQuery.value;
+  getData(fetchParams).then(cards => {
     if (cards.total === 0) {
       Notiflix.Notify.warning(
         'Sorry, there are no images matching your search query. Please try again.',
       );
+
       return;
     }
+
+    Notiflix.Notify.success(`Hooray! We found ${cards.total} images.`);
+
     if (page * PER_PAGE < cards.total) refs.loadMoreBtn.classList.toggle('load-more--hidden');
 
     drawTemplate(cards);
+    gallery.refresh();
   });
 };
 
 let loadMoreHandler = () => {
   page += 1;
-  getData(refs.form.elements.searchQuery.value, page).then(cards => {
+  fetchParams.q = refs.form.elements.searchQuery.value;
+  fetchParams.page = page;
+  getData(fetchParams).then(cards => {
     drawTemplate(cards);
+
     console.log(`page: ${page * PER_PAGE}  total: ${cards.total}`);
+
     if (page * PER_PAGE >= cards.total) {
       Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
       refs.loadMoreBtn.classList.toggle('load-more--hidden');
     }
+    gallery.refresh();
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
   });
 };
 
